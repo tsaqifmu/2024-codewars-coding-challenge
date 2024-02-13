619446530cf0d15134a5b9ac9bbf164f570f7875
Tsaqif Mushoddaq <mushoddaqt@gmail.com>
2024-02-14 05:38:42 +0700

    reduce but grow

---
+++ 1-grow.js
@@ -0,0 +1,16 @@
+// function grow(x) {
+//   let result = x[0];
+
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+
+//   return result;
+// }
+
+function grow(x) {
+  const result = x.reduce((acc, cur) => acc * cur, 1);
+  return result;
+}
+
+console.log(grow([1, 2, 3, 4])); // 3
