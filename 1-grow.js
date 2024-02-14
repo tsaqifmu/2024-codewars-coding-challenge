c54d33ba322cea9e7f4c776872a3be796a154980
Tsaqif Mushoddaq <mushoddaqt@gmail.com>
2024-02-14 17:17:36 +0700

    Refactor grow function to use arrow function and reduce method

--- 1-grow.js
+++ 1-grow.js
@@ -1,16 +1,3 @@
-// function grow(x) {
-//   let result = x[0];
+const grow = (x) => (result = x.reduce((acc, cur) => acc + cur, 1));
 
-//   for (let i = 1; i < x.length; i++) {
-//     result *= x[i];
-//   }
-
-//   return result;
-// }
-
-function grow(x) {
-  const result = x.reduce((acc, cur) => acc * cur, 1);
-  return result;
-}
-
-console.log(grow([1, 2, 3, 4])); // 3
+console.log(grow([1, 2, 3, 4]));
---
+++ 3-Is He_Gonna_Survive.js
@@ -0,0 +1,3 @@
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(10, 5));
