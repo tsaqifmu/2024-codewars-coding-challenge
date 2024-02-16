67e2ed53868a79a37c701c6a0aeae4f66260b218
Tsaqif Mushoddaq <mushoddaqt@gmail.com>
2024-02-16 20:21:53 +0700

    Update user authentication logic

---
+++ 10-ConvertaStringtoanArray.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// 10 / 366
+
+// Write a function to split a string and convert it into an array of words.
+
+const stringToArray = (string) => string.split(" ");
+
+console.log(stringToArray("Robin Singh"));
