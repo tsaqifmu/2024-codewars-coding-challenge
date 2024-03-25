4a56890ef837a006b4cd9b09c6c5adf805072e7b
Tsaqif Mushoddaq <mushoddaqt@gmail.com>
2024-03-25 20:32:20 +0700

    Update: function to handle edge cases

---
+++ 18-personalizedMessage.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+// 18 / 366
+
+// Create a function that gives a personalized greeting.
+// This function takes two parameters: name and owner.
+
+const greet = (name, owner) => (owner === name ? "Hello boss" : "Hello guest");
+
+console.log(greet("Greg", "Daniel"));
---
+++ 19-sumMixArray.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009
+
+// 19 / 366
+
+// Given an array of integers as strings and numbers,
+// return the sum of the array values as if all were numbers.
+
+// const sumMix = (x) => {
+//   let sum = 0;
+//   for (let i = 0; i < x.length; i++) {
+//     sum += +x[i];
+//   }
+//   return sum;
+// };
+
+const sumMix = (x) => x.reduce((acc, curr) => acc + +curr, 0);
+
+console.log(sumMix([9, 3, "7", "3"]));
